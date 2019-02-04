9b1a9a9cff661a9c98331ab74ec31b78210a66fe
jsphkm <jsphkm12@gmail.com>
2019-02-04 10:15:59 -0800

    Add redux store  - Install redux, react-redux, redux-logger, and redux-thunk  - Pass App through Provider with store  - Add combine redcuer with logger, thunk, and devtools  - Remove unnecessary code  - Change fullname input font-size  - Create a blank mainReducer

---
+++ src/reducers/main.js
@@ -0,0 +1,13 @@
+// import * as landing from '../actions/landing';
+
+const initialState = {
+  landing: {
+    fullname: 'Anonymous'
+  },
+}
+
+const mainReducer = (state=initialState, action) => {
+  return state;
+}
+
+export default mainReducer;
