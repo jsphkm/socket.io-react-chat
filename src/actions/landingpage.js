8f29f8d040214e65598d79a22b01ab0305221d8c
jsphkm <jsphkm12@gmail.com>
2019-02-09 21:25:07 -0800

    Add vertical sections and message input

---
+++ src/actions/landingpage.js
@@ -0,0 +1,5 @@
+export const ONCHANGE_FULLNAME = 'ONCHANGE_FULLNAME';
+export const updateFullName = (val) => ({
+  type: ONCHANGE_FULLNAME,
+  fullname: val
+});
---
+++ src/img/SendArrow.js
@@ -0,0 +1,36 @@
+import React from 'react';
+
+const SendArrow = () => (
+  <svg
+  width="84px"
+  height="95px"
+  viewBox="0 0 84 95"
+  version="1.1"
+  xmlns="http://www.w3.org/2000/svg"
+  >
+    <g
+    id="Symbols"
+    stroke="none"
+    strokeWidth="1"
+    fill="none"
+    fillRule="evenodd"
+    strokeLinecap="round"
+    >
+        <g
+        id="SendButton"
+        transform="translate(-44.000000, -39.000000)"
+        stroke="#FFFFFF"
+        strokeWidth="14"
+        >
+            <g id="SendArrow">
+                <g transform="translate(51.000000, 46.000000)">
+                    <polyline id="Path" strokeLinejoin="round" points="69.8516839 33.4800308 34.925842 1.86517468e-14 -7.03437308e-13 33.4800308"></polyline>
+                    <path d="M34.925842,0 L35.4684066,80.5285654" id="Path-2"></path>
+                </g>
+            </g>
+        </g>
+    </g>
+  </svg>
+)
+
+export default SendArrow;
