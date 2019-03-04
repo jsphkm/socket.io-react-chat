6d464d70a2685c637cf42eade2e0b8c73c1858f0
jsphkm <jsphkm12@gmail.com>
2019-03-04 08:11:19 -0800

    Fix timeout of 2000ms exceeded  - Call done() inside afterEach() and inside it()

--- test/socket-server.test.js
+++ test/socket-server.test.js
@@ -13,30 +13,31 @@ describe('Integration tests for server.js', function() {
   // };
   let server;
 
-  beforeEach(function() {
+  before(function() {
     server = startServer();
   });
 
-  afterEach(function(done) {
-    // return stopServer(server);
-    server.close(done);
+  after(function(done) {
+    stopServer(server);
+    done();
+    // server.close(done);
   });
 
   describe('GET', function() {
-    it('Should respond to /', function() {
-      return chai.request(server)
-        .get('/')
-        .then(res => {
-          expect(res).to.have.status(200);
-        }).catch(function(err) {
-          console.error('Get / has failed');
-        });
-    })
+    it('Should respond to /', (done) => {
+        chai.request(app)
+          .get('/')
+          .end((err, res) => {
+            expect(res).to.have.status(200);
+            done();
+          })
+      }
+    )
   })
 
   describe('NAMESPACE', function() {
-    it('Should connect to default namespace', function() {
-      return;
+    it('Should connect to default namespace', function(done) {
+      return done();
     });
   });
 });
